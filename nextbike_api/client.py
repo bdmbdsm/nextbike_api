1fa1018790061c4b0498b921c04bdd38ac901b91
Bohdan Dmytriv <dmytriv@quintagroup.org>
2019-07-11 12:11:05 +0300

    Add requests to all countries and to a specific one

--- nextbike_api/client.py
+++ nextbike_api/client.py
@@ -4,7 +4,7 @@ from nextbike_api.exceptions import NotAllowedQueryKey
 
 class Client:
 
-    _allowed_params = ('countries',)
+    _allowed_url_params = ('countries',)
 
     def __init__(self):
         self._base_url = 'https://api.nextbike.net/maps/nextbike-official.json'
@@ -16,7 +16,17 @@ class Client:
 
     def _check_allowed_query_keys(self, params):
         for param in params.keys():
-            if param not in self._allowed_params:
+            if param not in self._allowed_url_params:
                 raise NotAllowedQueryKey(
                     'Bad query param: {0}'.format(param)
                 )
+
+    def all_countries(self):
+        url = self._build_url()
+        resp = requests.get(url)
+        return resp.json()
+
+    def country(self, country_code):
+        url = self._build_url(countries=country_code)
+        resp = requests.get(url)
+        return resp.json()
